1beaca30b108cf31b1ee421b3e4c18c8ca781e01
Kyle <kyletrusler@gmail.com>
2025-11-21 16:32:07 -0500

    move

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Rockmill Magnesium",
+  title: "Rock Mill Magnesium",
   description: "Premium Climbing Route Management",
   manifest: "/manifest.json",
 };
